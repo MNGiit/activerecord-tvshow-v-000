99f69606e617ca235ad3f90e6d4088b77d02ef25
Mohammed Noori <kmanoorib@gmail.com>
2018-11-10 23:50:30 +0000

    automatically backed up by learn

--- db/migrate/002_add_season_to_shows.rb
+++ db/migrate/002_add_season_to_shows.rb
@@ -1,17 +1,5 @@
 class AddSeasonToShows < ActiveRecord::Migration[4.2]
   def change
-end
-
-
-class CreateShows < ActiveRecord::Migration[4.2]
-  
-  def changes
-    create_table :shows do |t|
-      t.string :name
-      t.string :network
-      t.string :day
-      t.integer :rating
-    end
+    
   end
-  
 end
