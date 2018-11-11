6f1049da9804a45513ff1bd85753c9a2fb7a9c0a
Mohammed Noori <kmanoorib@gmail.com>
2018-11-11 00:16:04 +0000

    automatically backed up by learn

--- app/models/show.rb
+++ app/models/show.rb
@@ -2,7 +2,7 @@ class Show < ActiveRecord::Base
   
   def self.highest_rating
     # hint: if there is a minimum Active Record method, might there be a maximum method?
-    
+    self.maximum(:rating)
   end
   
 end
